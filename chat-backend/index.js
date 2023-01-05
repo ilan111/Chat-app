1f8b2d310b13a481462818a297fe725f299aa126
Ilan Y <ilanydg@gmail.com>
2023-01-05 12:17:56 +0200

    added config folder with app.js module for environment vars

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
-const app = express();
+const config = require("./config/app");
 
-const port = 3000;
+const app = express();
 
 app.get("/home", (req, res) => {
   return res.send("Home screen");
 });
 
+app.get("/login", (req, res) => {
+  return res.send("Login screen");
+});
+
+const port = config.appPort;
+
 app.listen(port, () => {
-  console.log(`Server listening on port $(port)`);
+  console.log(`Server listening on port ${port}`);
 });
 
 console.log("Hello World!!");
