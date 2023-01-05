e8e3d7abcb4c4dbcd223f15aa0b1d84f3eb41f6d
Ilan Y <ilanydg@gmail.com>
2023-01-05 11:46:36 +0200

    added express app

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -1,1 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/home", (req, res) => {
+  return res.send("Home screen");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port $(port)`);
+});
+
 console.log("Hello World!!");
